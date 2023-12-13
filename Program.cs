d7dfc90a14638e04ea0d0d7328cce4f8d20a0cbf
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-12-13 10:20:29 -0700

    prompted the user to enter name and read input

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ProceduralProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your Name?");
+            var name = Console.ReadLine();
+            var reversed = ReverseName(name);
+            Console.WriteLine("Reversed name: " + reversed);   
+        }
+
+        public static string ReverseName(string name)
+        {
+            var array = new char[name.Length];
+             for (var i = name.Length; i > 0; i--)
+                array[name.Length - i] = name[i - 1];
+
+          return new string(array);
+
+        }
+    }
+}
